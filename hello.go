fe76617037bf1f161ffbe1dd730b3f546839c3fc
takara1356 <goblin7283@gmail.com>
2019-12-30 23:20:05 +0900

    #03

--- hello.go
+++ hello.go
@@ -3,5 +3,19 @@ package main
 import "fmt"
 
 func main()  {
-	fmt.Println("hello world")
+	// var msg string
+	// msg = "hello world"
+	msg := "hello world"
+	fmt.Println(msg)
+
+	// var a , b int
+	// a, b = 10, 15
+	a, b := 10,15
+
+	var (
+		c int
+		d string
+	)
+	c = 20
+	d = "hoge"
 }
