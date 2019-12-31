2aefc2c609a14fe2fd5dd4ba2dfc7c34e5d0ec03
takara1356 <goblin7283@gmail.com>
2019-12-31 10:25:36 +0900

    #07

--- hello.go
+++ hello.go
@@ -3,14 +3,9 @@ package main
 import "fmt"
 
 func main()  {
-	// const name = "takara"
-	// name = "tanaka"
-	// fmt.Println
-
-	const (
-		sun = iota
-		mon 
-		tue
-	)
-	fmt.Println(sun, mon ,tue)
+	a := 5
+	var pa *int
+	pa = &a
+	fmt.Println(pa)
+	fmt.Println(*pa)
 }
